08e4e734d89263dfc7cf6a3b1603904e5d436fa6
Estella <estellallegra@gmail.com>
2017-07-12 10:06:29 -0700

    My Django Girls app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,4 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-ALLOWED_HOSTS = ['127.0.0.1', '<Girls Who Code>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'estellaallegra.pythonanywhere.com']
